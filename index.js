71f3d538befabd56ffe14b66a258d4e08e9ec34c
BryteLitty <littyofficials@gmail.com>
2020-09-29 04:37:17 +0100

    front-end done

--- index.js
+++ index.js
@@ -53,4 +53,32 @@ $(document).ready(function(){
             }
         }
     });
-});
+
+    // Product qty section
+    let $qty_up = $(".qty .qty-up");
+    let $qty_down = $(".qty .qty-down");
+    // let $input = $(".qty .qty_input");
+
+    // add event listener
+    $qty_up.click(function(e){
+        let $input = $(`.qty_input[data-id='${$(this).data("id")}']`);
+        if($input.val() >= 1 &&  $input.val() <= 9){
+            $input.val(function(i,oldval){
+                return ++oldval;
+            });
+        }
+    });
+
+
+    $qty_down.click(function(e){
+        let $input = $(`.qty_input[data-id="${$(this).data("id")}"]`);
+        if($input.val() > 1 &&  $input.val() <= 10){
+            $input.val(function(i,oldval){
+                return --oldval;
+            });
+        }
+    });
+
+    
+
+}); 
